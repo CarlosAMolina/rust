c1d7b43152ee0d78f8f15a9ae5d64b20ca2c07f8
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-06-09 22:22:01 +0200

    Add performance regex for python and rust

---
+++ regex/performance-python/main.py
@@ -0,0 +1,102 @@
+from timeit import default_timer as timer
+from typing import List, Optional
+import re
+
+# https://stackoverflow.com/questions/7370801/how-to-measure-elapsed-time-in-python#7370824
+
+
+class Log:
+
+    def __init__(
+        self,
+        remote_addr: str,
+        remote_user: str,
+        time_local: str,
+        request: str,
+        status: str,
+        body_bytes_sent: str,
+        http_referer: str,
+        http_user_agent: str,
+    ):
+        self.remote_addr = remote_addr
+        self.remote_user = remote_user
+        self.time_local = time_local
+        self.request = request
+        self.status = status
+        self.body_bytes_sent = body_bytes_sent
+        self.http_referer = http_referer
+        self.http_user_agent = http_user_agent
+
+    def __repr__(self):
+        return "{},{},{},{},{},{},{},{}".format(
+            self.remote_addr,
+            self.remote_user,
+            self.time_local,
+            self.request,
+            self.status,
+            self.body_bytes_sent,
+            self.http_referer,
+            self.http_user_agent,
+        )
+
+# https://docs.nginx.com/nginx/admin-guide/monitoring/logging/
+REGEX = re.compile(
+    r"""
+    ^
+    ((\d{1,3}[\.]){3}\d{1,3}) # IPv4
+    \s-\s
+    (.+)                      # Remote user
+    \s\[
+    (.+)                      # Time local
+    \]\s"
+    (.*)                      # Request
+    "\s
+    (\d{1,3})                 # Status
+    \s
+    (\d+)                     # Body bytes sent
+    \s"
+    (.+)                      # HTTP referer
+    "\s"
+    (.+)                      # HTTP user agent
+    "
+""",
+    re.VERBOSE,
+)
+
+
+def get_log(line: str) -> Optional[Log]:
+    match = re.search(REGEX, line)
+    return (
+        None
+        if match is None
+        else Log(
+            remote_addr=match.group(1),
+            remote_user=match.group(3),
+            time_local=match.group(4),
+            request=match.group(5),
+            status=match.group(6),
+            body_bytes_sent=match.group(7),
+            http_referer=match.group(8),
+            http_user_agent=match.group(9),
+        )
+    )
+
+
+def run():
+    start = timer()
+    for i in range(5000):
+        log = (
+            '8.8.8.8 - abc [28/Nov/2021:00:18:22 +0100] "GET / HTTP/1.1" 200 77 "-"'
+            ' "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36'
+            ' (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"'
+        )
+        result = get_log(log)
+        print(result)
+    end = timer()
+    duration = end - start
+    #print(f"Time elapsed: {duration}s")
+    print(f"Time elapsed: {duration * 1000}ms")
+
+
+if __name__ == "__main__":
+    run()
---
+++ regex/performance/src/main.rs
@@ -0,0 +1,118 @@
+use std::fmt;
+use std::time::Instant;
+
+use lazy_static::lazy_static;
+use regex::Regex;
+
+fn main() {
+    let start = Instant::now();
+    println!("Hello, world!");
+    let log = "8.8.8.8 - - [28/Oct/2021:00:18:22 +0100] \"GET / HTTP/1.1\" 200 77 \"-\" \"foo bar 1\"";
+    for _ in 1..5000 {
+        let result = get_regex_result(&log);
+        println!("{:?}", result);
+    }
+    let duration = start.elapsed();
+    println!("Time elapsed: {:?}", duration);
+}
+
+#[derive(Debug, PartialEq)]
+struct Log<'a> {
+    remote_addr: &'a str,
+    remote_user: &'a str,
+    time_local: &'a str,
+    request: &'a str,
+    status: &'a str,
+    body_bytes_sent: &'a str,
+    http_referer: &'a str,
+    http_user_agent: &'a str,
+}
+
+impl<'a> fmt::Display for Log<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{},{},{},{},{},{},{},{}",
+            self.remote_addr,
+            self.remote_user,
+            self.time_local,
+            self.request,
+            self.status,
+            self.body_bytes_sent,
+            self.http_referer,
+            self.http_user_agent,
+        )
+    }
+}
+
+fn get_regex_result(text: &str) -> Option<Log> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(
+            r#"(?x)
+          ^
+          ((\d{1,3}[\.]){3}\d{1,3}) # IPv4
+          \s-\s
+          (.+)                      # Remote user
+          \s\[
+          (.+)                      # Time local
+          \]\s"
+          (.*)                      # Request
+          "\s
+          (\d{1,3})                 # Status
+          \s
+          (\d+)                     # Body bytes sent
+          \s"
+          (.+)                      # HTTP referer
+          "\s"
+          (.+)                      # HTTP user agent
+          "
+          $
+        "#,
+        )
+        .unwrap();
+    }
+    RE.captures(text).and_then(|cap| {
+        let groups = (
+            cap.get(1),
+            cap.get(3),
+            cap.get(4),
+            cap.get(5),
+            cap.get(6),
+            cap.get(7),
+            cap.get(8),
+            cap.get(9),
+        );
+        match groups {
+            (
+                Some(remote_addr),
+                Some(remote_user),
+                Some(time_local),
+                Some(request),
+                Some(status),
+                Some(body_bytes_sent),
+                Some(http_referer),
+                Some(http_user_agent),
+            ) => Some(Log {
+                remote_addr: remote_addr.as_str(),
+                remote_user: remote_user.as_str(),
+                time_local: time_local.as_str(),
+                request: request.as_str(),
+                status: status.as_str(),
+                body_bytes_sent: body_bytes_sent.as_str(),
+                http_referer: http_referer.as_str(),
+                http_user_agent: http_user_agent.as_str(),
+            }),
+            _ => None,
+        }
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_regex_result() {
+        assert_eq!(8, get_regex_result());
+    }
+}
