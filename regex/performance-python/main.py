d110f702f2e1a0e14f7b77eadd384d940f4fe372
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-06-21 22:59:11 +0200

    Python performance add result without regex

--- regex/performance-python/main.py
+++ regex/performance-python/main.py
@@ -81,21 +81,67 @@ def get_log(line: str) -> Optional[Log]:
         )
     )
 
+def get_result_without_regex_one_loop(text: str) -> Log:
+    log_parts_index = [0]
+    characters_to_match = [
+        ' ',
+        ' ',
+        '[',
+        ']',
+        '"',
+        '"',
+        ' ',
+        ' ',
+        '"',
+        '"',
+        '"',
+    ]
+    match_index = 0
+    text_index = 0
+    for item in text:
+        if item == characters_to_match[match_index]:
+            log_parts_index.append(text_index)
+            if match_index < len(characters_to_match) - 1:
+                match_index += 1
+        text_index += 1
+    return Log(
+        remote_addr= text[log_parts_index[0]:log_parts_index[1]],
+        remote_user= text[log_parts_index[2] + 1:log_parts_index[3] - 1],
+        time_local= text[log_parts_index[3] + 1:log_parts_index[4]],
+        request= text[log_parts_index[5] + 1:log_parts_index[6]],
+        status= text[log_parts_index[7] + 1: log_parts_index[8]],
+        body_bytes_sent= text[log_parts_index[8] + 1: log_parts_index[9] - 1],
+        http_referer= text[log_parts_index[9] + 1:log_parts_index[10]],
+        http_user_agent= text[log_parts_index[11] + 1:log_parts_index[12]],
+    )
+
 
 def run():
+    log = (
+        '8.8.8.8 - abc [28/Nov/2021:00:18:22 +0100] "GET / HTTP/1.1" 200 77 "-"'
+        ' "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36'
+        ' (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"'
+    )
+    loops_number = 5_000
     start = timer()
-    for i in range(5_000):
-        log = (
-            '8.8.8.8 - abc [28/Nov/2021:00:18:22 +0100] "GET / HTTP/1.1" 200 77 "-"'
-            ' "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36'
-            ' (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"'
-        )
+    for i in range(loops_number):
         result = get_log(log)
         #print(result)
     end = timer()
     duration = end - start
     #print(f"Time elapsed: {duration}s")
-    print(f"Time elapsed: {duration * 1000}ms")
+    print(f"Time elapsed groups: {duration * 1000}ms")
+    start = timer()
+    for i in range(loops_number):
+        result = get_result_without_regex_one_loop(log)
+        #print(result)
+    end = timer()
+    duration = end - start
+    #print(f"Time elapsed: {duration}s")
+    print(f"Time elapsed without regex: {duration * 1000}ms")
+
+
+
 
 
 if __name__ == "__main__":
