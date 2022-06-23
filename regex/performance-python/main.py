7e6cf9c9e453b07ce19f62b4e60cdb78373e5524
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-06-23 20:55:55 +0200

    Python performance add regex match

--- regex/performance-python/main.py
+++ regex/performance-python/main.py
@@ -39,6 +39,64 @@ class Log:
         )
 
 
+#def get_result_with_regex_math(text: str) -> Log:
+#    characters_checked = 0;
+#    lazy_static! {
+#        static ref RE_IPV4: Regex = Regex::new(r"(\d{1,3}[\.]){3}\d{1,3}",).unwrap();
+#    }
+#    let mut log_parts_index = vec![0];
+#    let mut re_result = RE_IPV4.find(text).unwrap();
+#    log_parts_index.push(re_result.end());
+#    characters_checked += re_result.end();
+#    characters_checked += 3;
+#    log_parts_index.push(characters_checked);
+#    lazy_static! {
+#        static ref RE_REMOTE_USER: Regex = Regex::new(r".+\s\[").unwrap();
+#    }
+#    re_result = RE_REMOTE_USER.find(&text[characters_checked..]).unwrap();
+#    characters_checked += re_result.end();
+#    log_parts_index.push(characters_checked - 2);
+#    lazy_static! {
+#        static ref RE_TIME_LOCAL: Regex =
+#            Regex::new(r"\d{2}/[[:alpha:]]{3}/\d{4}:\d{2}:\d{2}:\d{2}\s\+\d{4}",).unwrap();
+#    }
+#    log_parts_index.push(characters_checked);
+#    re_result = RE_TIME_LOCAL.find(&text[characters_checked..]).unwrap();
+#    characters_checked += re_result.end();
+#    log_parts_index.push(characters_checked);
+#    characters_checked += 3;
+#    log_parts_index.push(characters_checked);
+#    lazy_static! {
+#        static ref RE_REQUEST: Regex = Regex::new(r#"[[:alpha:]].*"\s\d"#).unwrap();
+#    }
+#    re_result = RE_REQUEST.find(&text[characters_checked..]).unwrap();
+#    characters_checked += re_result.end() - 1;
+#    log_parts_index.push(characters_checked - 2);
+#    log_parts_index.push(characters_checked);
+#    //lazy_static! {
+#    //    static ref RE_STATUS: Regex = Regex::new(r"\d{3}").unwrap();
+#    //}
+#    //let status = RE_STATUS.find(&text[characters_checked..]).unwrap();
+#    //characters_checked += status.end();
+#    characters_checked += 3;
+#    log_parts_index.push(characters_checked);
+#    log_parts_index.push(characters_checked + 1);
+#    lazy_static! {
+#        static ref RE_BODY_BYTES_SENT: Regex = Regex::new(r"\d{1,3}").unwrap();
+#    }
+#    let body_bytes_sent = RE_BODY_BYTES_SENT
+#        .find(&text[characters_checked..])
+#        .unwrap();
+#    characters_checked += body_bytes_sent.end();
+#    log_parts_index.push(characters_checked);
+#
+
+
+
+
+
+
+
 # https://docs.nginx.com/nginx/admin-guide/monitoring/logging/
 REGEX = re.compile(
     r"""
@@ -64,7 +122,25 @@ REGEX = re.compile(
 )
 
 
-def get_log_with_regex_groups(line: str) -> Optional[Log]:
+def get_log_with_regex_match_groups(line: str) -> Optional[Log]:
+    match = re.match(REGEX, line)
+    return (
+        None
+        if match is None
+        else Log(
+            remote_addr=match.group(1),
+            remote_user=match.group(3),
+            time_local=match.group(4),
+            request=match.group(5),
+            status=match.group(6),
+            body_bytes_sent=match.group(7),
+            http_referer=match.group(8),
+            http_user_agent=match.group(9),
+        )
+    )
+
+
+def get_log_with_regex_search_groups(line: str) -> Optional[Log]:
     match = re.search(REGEX, line)
     return (
         None
@@ -126,12 +202,20 @@ def run():
     loops_number = 5_000
     start = timer()
     for i in range(loops_number):
-        result = get_log_with_regex_groups(log)
+        result = get_log_with_regex_match_groups(log)
+        #print(result)
+    end = timer()
+    duration = end - start
+    # print(f"Time elapsed: {duration}s")
+    print(f"Time elapsed match groups: {duration * 1000}ms")
+    start = timer()
+    for i in range(loops_number):
+        result = get_log_with_regex_search_groups(log)
         # print(result)
     end = timer()
     duration = end - start
     # print(f"Time elapsed: {duration}s")
-    print(f"Time elapsed groups: {duration * 1000}ms")
+    print(f"Time elapsed search groups: {duration * 1000}ms")
     start = timer()
     for i in range(loops_number):
         result = get_result_without_regex_one_loop(log)
