780a8734e886cdb57ccf676419adf54564456dab
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-07-03 22:07:07 +0200

    Improve rust read .gz

--- read_files/gz/rust/read_gz/src/main.rs
+++ read_files/gz/rust/read_gz/src/main.rs
@@ -1,23 +1,56 @@
-use std::time::Instant;
-use std::io::{BufRead, BufReader};
-use std::fs::File;
+use flate2::read::GzDecoder;
 use libflate::gzip::Decoder;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::io::prelude::*;
+use std::time::Instant;
 
 fn main() {
-    let start = Instant::now();
-    let path = "src/access.log.10.gz";
-    process_file(path);
-    println!("Time elapsed {:?}", start.elapsed());
+    run_process(process_file_with_decoder_and_lines, "decoder and lines");
+    run_process(process_file_with_gz_decoder_and_lines, "gz_decoder and lines");
+    run_process(process_file_with_gz_decoder_and_read_to_string, "gz_decoder and read_to_string");
 }
 
-fn process_file(path: &str) {
+fn run_process(function: fn(&str), parse_description: &str) {
+    let path = "src/access.log.10.gz";
+    let start = Instant::now();
+    function(path);
+    println!("Time elapsed with {:?}: {:?}", parse_description, start.elapsed());
+}
+
+fn process_file_with_decoder_and_lines(path: &str) {
     // https://stackoverflow.com/questions/66250215/rust-and-gzipped-files
     let file = File::open(path).unwrap();
     let mut lines_number = 0;
     for line in BufReader::new(Decoder::new(file).unwrap())
         .lines() {
-            println!("{:?}", line);
+            let _result = line.unwrap();
+            //println!("{}", _result);
             lines_number += 1;
         }
     println!("Number of lines: {}", lines_number);
 }
+
+fn process_file_with_gz_decoder_and_lines(path: &str) {
+    // https://stackoverflow.com/questions/66250215/rust-and-gzipped-files
+    let file = File::open(path).unwrap();
+    let mut lines_number = 0;
+    for line in BufReader::new(GzDecoder::new(file))
+        .lines() {
+            let _result = line.unwrap();
+            //println!("{}", _result);
+            lines_number += 1;
+        }
+    println!("Number of lines: {}", lines_number);
+}
+
+fn process_file_with_gz_decoder_and_read_to_string(path: &str) {
+    // https://rust-lang-nursery.github.io/rust-cookbook/compression/tar.html#decompress-a-tarball
+    let file = File::open(&path).unwrap();
+    let mut d = GzDecoder::new(file);
+    let mut s = String::new();
+
+    d.read_to_string(&mut s).unwrap();
+    //println!("{}", s);
+}
+
