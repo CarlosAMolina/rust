646cf27132ecb3496ff5ef17847decb61243261e
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-12-27 11:38:48 +0100

    Apply cargo fmt

--- file_size/file_size/src/main.rs
+++ file_size/file_size/src/main.rs
@@ -6,10 +6,8 @@ fn main() {
     assert_eq!(446, file_size);
 }
 
-
 fn get_file_size(file_path_name: &str) -> std::io::Result<u64> {
     let f = File::open(file_path_name)?;
     let file_size = f.metadata().unwrap().len();
     Ok(file_size)
 }
-
