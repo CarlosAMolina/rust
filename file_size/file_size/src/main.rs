1921342ad857179af15bd4bddd2c196ccdff3bfa
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-12-27 11:38:15 +0100

    Refactor move function get_file_size

--- file_size/file_size/src/main.rs
+++ file_size/file_size/src/main.rs
@@ -1,10 +1,15 @@
 use std::fs::File;
 
-fn main() -> std::io::Result<()> {
+fn main() {
     let file_path_name = "../file-to-get-size.txt";
+    let file_size = get_file_size(file_path_name).unwrap();
+    assert_eq!(446, file_size);
+}
+
+
+fn get_file_size(file_path_name: &str) -> std::io::Result<u64> {
     let f = File::open(file_path_name)?;
     let file_size = f.metadata().unwrap().len();
-    assert_eq!(446, file_size);
-    Ok(())
+    Ok(file_size)
 }
 
