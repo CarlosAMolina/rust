eb5033c7ebd4153e8ac0cf1d491737bf190d620f
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-12-27 11:11:19 +0100

    Add primitive_date_time assertion

--- datetime/primitive_date_time/primitive_date_time/src/main.rs
+++ datetime/primitive_date_time/primitive_date_time/src/main.rs
@@ -3,7 +3,11 @@ use time::macros::datetime;
 
 fn main() {
     assert_eq!(
-        datetime!(2019 - 11 - 26 18:30),
-        datetime!(2019 - 11 - 25 15:30).saturating_add(27.hours())
+        datetime!(2019 - 11 - 25 15:30).saturating_add(27.hours()),
+        datetime!(2019 - 11 - 26 18:30)
+    );
+    assert_eq!(
+        datetime!(2019 - 11 - 26 18:30:59).saturating_add(1.seconds()),
+        datetime!(2019 - 11 - 26 18:31:00),
     );
 }
