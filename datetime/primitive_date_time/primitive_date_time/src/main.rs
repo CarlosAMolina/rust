e92afc7799180926babbf0bf6c63540dd182742c
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2022-12-30 11:22:47 +0100

    Add date format_description

--- datetime/primitive_date_time/primitive_date_time/src/main.rs
+++ datetime/primitive_date_time/primitive_date_time/src/main.rs
@@ -1,4 +1,5 @@
 use time::ext::NumericalDuration;
+use time::format_description;
 use time::macros::datetime;
 
 fn main() {
@@ -9,5 +10,15 @@ fn main() {
     assert_eq!(
         datetime!(2019 - 11 - 26 18:30:59).saturating_add(1.seconds()),
         datetime!(2019 - 11 - 26 18:31:00),
+    );
+    test_format_description();
+}
+
+fn test_format_description() {
+    let format =
+        format_description::parse("[year]-[month]-[day] [hour]:[minute]:[second]").unwrap();
+    assert_eq!(
+        datetime!(2020-01-02 03:04:05).format(&format).unwrap(),
+        "2020-01-02 03:04:05"
     );
 }
