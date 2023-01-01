5dc33d3ce4443833063c64f276ba4463d072d4a6
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-01-01 13:15:59 +0100

    Fix compress gz to be decompressed with gunzip comand

--- compress/file-as-gz/compress-file-as-gz/src/main.rs
+++ compress/file-as-gz/compress-file-as-gz/src/main.rs
@@ -1,16 +1,40 @@
 use flate2::write::GzEncoder;
 use flate2::Compression;
 use std::fs::File;
+use std::fs;
 use tar;
+use std::io::prelude::*;
+use flate2::write::ZlibEncoder;
+
 
 fn main() -> Result<(), std::io::Error> {
-    let path_name_compressed_file = "/tmp/file.gz";
+    let path_name_compressed_file = "/tmp/test/error-new.log.9.gz";
     let tar_gz = File::create(path_name_compressed_file)?;
-    let enc = GzEncoder::new(tar_gz, Compression::default());
+    let mut enc = GzEncoder::new(tar_gz, Compression::default());
+
+    enc.write_all(b"Hello World")?;
+    enc.finish()?;
+
+    /*
+    let compressed = enc.finish()?;
+    println!("{contents}");
+
+    
     let mut tar = tar::Builder::new(enc);
-    let path_name_file_to_compress = "../files/foo.txt";
-    let path_name_file_to_create_in_compressed_file = "result/files/foo.txt";
+    let path_name_file_to_compress = "/tmp/test/error-original.log.9";
+    let path_name_file_to_create_in_compressed_file = "error-new.log.9";
     let mut f = File::open(path_name_file_to_compress)?;
     tar.append_file(path_name_file_to_create_in_compressed_file, &mut f)?;
+
+    match file.write_all(e) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+
+    let contents = fs::read_to_string(path_name_file_to_compress).expect("Should have been able to read the file");
+    println!("{contents}");
+    */
+
+
     Ok(())
 }
