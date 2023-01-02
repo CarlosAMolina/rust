5cc3fd1031fbaa3c8b0cb6ba1df3ca0757d62142
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-01-02 03:13:03 +0100

    Add write to file all lines at once

--- write-to-file/fastest-method/write-to-file/src/main.rs
+++ write-to-file/fastest-method/write-to-file/src/main.rs
@@ -5,14 +5,19 @@ use std::path::Path;
 use std::time::Instant;
 
 pub fn main() -> Result<(), Box<dyn Error>> {
+    let line_to_write = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n";
     let start = Instant::now();
-    write_one_line_in_each_loop()?;
+    write_one_line_in_each_loop(line_to_write)?;
     let duration = start.elapsed();
-    println!("Time elapsed: {:?}", duration);
+    println!("Time elapsed (write one line in each loop): {:?}", duration);
+    let start = Instant::now();
+    write_all_lines_at_once(line_to_write)?;
+    let duration = start.elapsed();
+    println!("Time elapsed (write all lines at once): {:?}", duration);
     Ok(())
 }
 
-fn write_one_line_in_each_loop() -> Result<(), Box<dyn Error>> {
+fn write_one_line_in_each_loop(line_to_write: &str) -> Result<(), Box<dyn Error>> {
     let file_path_name = "/tmp/file-1.txt";
     let path = Path::new(&file_path_name);
     let display = path.display();
@@ -24,11 +29,32 @@ fn write_one_line_in_each_loop() -> Result<(), Box<dyn Error>> {
         Ok(file) => file,
     };
     for _ in 0..1_000_000 {
-        let text_to_write = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n";
-        if let Err(why) = file.write_all(text_to_write.as_bytes()) {
+        if let Err(why) = file.write_all(line_to_write.as_bytes()) {
             let error_msg = format!("couldn't write to {}: {}", display, why);
             return Err(error_msg.into());
         }
+    }
+    Ok(())
+}
+
+fn write_all_lines_at_once(line_to_write: &str) -> Result<(), Box<dyn Error>> {
+    let file_path_name = "/tmp/file-2.txt";
+    let path = Path::new(&file_path_name);
+    let display = path.display();
+    let mut file = match File::create(path) {
+        Err(why) => {
+            let error_msg = format!("couldn't create {}: {}", display, why);
+            return Err(error_msg.into());
+        }
+        Ok(file) => file,
+    };
+    let mut text_to_write_all = String::new();
+    for _ in 0..1_000_000 {
+        text_to_write_all.push_str(line_to_write);
+    }
+    if let Err(why) = file.write_all(text_to_write_all.as_bytes()) {
+        let error_msg = format!("couldn't write to {}: {}", display, why);
+        return Err(error_msg.into());
     }
     Ok(())
 }
