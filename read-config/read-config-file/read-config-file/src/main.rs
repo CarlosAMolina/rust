c6098ae3b3dd79baf94fbf72a1a22ff8ac3d9c07
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-07-02 19:04:29 +0200

    Add read config using a config file

---
+++ read-config/read-config-file/read-config-file/src/main.rs
@@ -0,0 +1,23 @@
+use config::Config;
+
+fn main() {
+    #[derive(serde::Deserialize)]
+    struct Args {
+        /// URL for the postgres database
+        database_host: String,
+        /// PORT number for the database connection
+        database_port: u16,
+    }
+    // It is not required to specify the .toml extension when parsing the file.
+    let config = Config::builder()
+        .add_source(config::File::with_name("setup"))
+        .build()
+        .unwrap();
+    let config =  config
+        .try_deserialize::<Args>()
+        .unwrap();
+    println!("{}", format!(
+        "Dabase host: {}. Dabase port: {}",
+        config.database_host, config.database_port
+    ))
+}
