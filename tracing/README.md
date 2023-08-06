1323c7614faa795d6bbaf3056608f5c42734a1f4
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-08-07 00:01:28 +0200

    Add tracing

---
+++ tracing/README.md
@@ -0,0 +1,9 @@
+## Resources
+
+Span and event:
+
+<https://docs.rs/tracing/latest/tracing/index.html>
+
+Subscriber:
+
+<https://github.com/tokio-rs/tracing>
---
+++ tracing/tracing/src/main.rs
@@ -0,0 +1,54 @@
+use tracing::{event, instrument, span, Level};
+
+#[derive(Debug)]
+pub struct User {
+    pub name: String,
+    pub email: String,
+}
+
+fn main() {
+    // In order to record trace events, executables have to use a Subscriber implementation compatible with tracing. A Subscriber implements a way of collecting trace data, such as by logging it to standard output.
+    // Any trace events generated outside the context of a subscriber will not be collected.
+    // Install global collector configured based on RUST_LOG env var.
+    // Using init() calls set_global_default() so this collector will be used as the default in all threads for the remainder of the duration of the program, similar to how loggers work in the log crate.
+    tracing_subscriber::fmt::init();
+
+    // this creates a new event, outside of any spans.
+    tracing::info!(
+        answer = 42,
+        summary = "this creates a new event, outside of any spans"
+    );
+
+    // Construct a new span named "my span" with trace log level.
+    // Equals as `let span = info_span!("my_span");`.
+    let span = span!(Level::INFO, "my_span");
+    // Enter the span, returning a guard object.
+    let _guard = span.enter();
+    // Any trace events that occur before the guard is dropped will occur
+    // within "my_span".
+    event!(Level::DEBUG, "text debug"); // Not showed as span has INFO level.
+    event!(Level::INFO, "text info");
+    // Dropping the guard will exit the span.
+
+    my_function(3);
+
+    // Log structs.
+    let user = User {
+        name: "ferris".to_string(),
+        email: "ferris@rust-lang.org".to_string(),
+    };
+    // ?: specifies something should be recorded using its fmt::Debug implementation.
+    event!(Level::INFO, text = ?user);
+    // %: specifies something should be recorded using its fmt::Display implementation:
+    event!(Level::INFO, text = %user.email);
+}
+
+// #[instrument]: adds tracing spans to functions
+// Creates and enter a span with the function’s name every time the function is called.
+// Arguments to that function will be recorded as span fields using fmt::Debug.
+#[instrument]
+pub fn my_function(my_arg: usize) {
+    // This event will be recorded inside a span named `my_function` with the
+    // field `my_arg`.
+    event!(Level::INFO, "inside my_function!");
+}
