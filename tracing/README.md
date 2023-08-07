335e9750686f83ff7e4f6c09d8978489f602a6ad
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-08-07 23:33:31 +0200

    Fix show debug events

--- tracing/README.md
+++ tracing/README.md
@@ -8,8 +8,13 @@ Subscriber:
 
 <https://github.com/tokio-rs/tracing>
 
+  with_max_level:
+
+  <https://docs.rs/tracing-subscriber/latest/tracing_subscriber/fmt/struct.SubscriberBuilder.html>
+
 Write to file:
 
 <https://docs.rs/tracing-appender/latest/tracing_appender/>
 <https://docs.rs/tracing-appender/latest/tracing_appender/rolling/struct.RollingFileAppender.html>
 <https://docs.rs/tracing-appender/latest/tracing_appender/non_blocking/struct.WorkerGuard.html>
+
--- tracing/tracing/src/main.rs
+++ tracing/tracing/src/main.rs
@@ -18,37 +18,49 @@ fn main() {
     // Using init() calls set_global_default() so this collector will be used as the default in all threads for the remainder of the duration of the program, similar to how loggers work in the log crate.
     // Blocking
     // Log specified level and above to stdout.
-    //let stdout = std::io::stdout.with_max_level(tracing::Level::TRACE);
-    //tracing_subscriber::fmt()
-    //// Combine the stdout and log file `MakeWriter`s into one
-    //// `MakeWriter` that writes to both
-    //.with_writer(stdout.and(logfile))
-    //.init();
+    // This must be added to tracing_subscriber as `with_writer(stdout)`
+    // tracing::Level | will show
+    // ---------------|---------------------------------
+    // ERROR          | ERROR
+    // WARN           | ERROR, WARN
+    // INFO           | ERROR, WARN, INFO
+    // DEBUG          | ERROR, WARN, INFO, DEBUG
+    // TRACE          | ERROR, WARN, INFO, DEBUG, TRACE
+    //let blocking_stdout = std::io::stdout.with_max_level(tracing::Level::TRACE);
     // Non Blocking
     // WorkerGuard should be assigned in the main function or whatever the entrypoint of the program is. This will ensure that the guard will be dropped during an unwinding or when main exits successfully.
     let (non_blocking_logfile, _guard_logfile) = tracing_appender::non_blocking(logfile);
     let (non_blocking_stdout, _guard_stdout) = tracing_appender::non_blocking(std::io::stdout());
     tracing_subscriber::fmt()
+    .with_max_level(tracing_subscriber::filter::LevelFilter::DEBUG)
+    // Combine the stdout and log file `MakeWriter`s into one
+    // `MakeWriter` that writes to both
+    // Despite `stdout` hast level TRACE, the subscriber has DEBUG and
+    // the most restricive one is applied.
+    //.with_writer(blocking_stdout)
+    //.with_writer(blocking_stdout.and(logfile))
     .with_writer(non_blocking_logfile.and(non_blocking_stdout))
     .init();
 
-    // this creates a new event, outside of any spans.
+    // this creates new events, outside of any spans.
+    // The TRACE is not showed as the DEBUG level applies.
+    event!(Level::TRACE, "trace event, outside of any spans");
+    event!(Level::DEBUG, "debug event, outside of any spans");
     tracing::info!(
         answer = 42,
-        summary = "this creates a new event, outside of any spans"
+        summary = "info event, outside of any spans"
     );
-
     // Construct a new span named "my span" with trace log level.
     // Equals as `let span = info_span!("my_span");`.
-    let span = span!(Level::WARN, "my_span"); // TODO check whty Level is not applied, maybe
-                                              // tracing_appender causes interference.
+    // TODO despite using Level DEBUG or ERROR, all events are in my_span.
+    // Despite Level is specified, the Level in the subscriber applies.
+    let span = span!(Level::WARN, "my_span");
     // Enter the span, returning a guard object.
     let _guard = span.enter();
     // Any trace events that occur before the guard is dropped will occur
     // within "my_span".
-    // TODO try to output TRACE nad DEBUG event, i can't
-    event!(Level::TRACE, "text trace"); // Not showed as span has INFO level.
-    event!(Level::DEBUG, "text debug"); // Not showed as span has INFO level.
+    event!(Level::TRACE, "text trace"); // Not showed.
+    event!(Level::DEBUG, "text debug");
     event!(Level::INFO, "text info");
     event!(Level::WARN, "text warning");
     event!(Level::ERROR, "text error");
