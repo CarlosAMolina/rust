0135c9c6c9cf6a5a26f93347c9d1b4bcb39d88a9
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-08-23 23:40:37 +0200

    Refactor extract methods and add print when the method is called

--- warp/hello-world/hello-world/src/main.rs
+++ warp/hello-world/hello-world/src/main.rs
@@ -1,14 +1,31 @@
-use warp::Filter;
+use warp::{Filter, Reply};
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
+    let routes = build_routes().await;
     let ip = [127, 0 , 0, 1];
     let port = 3030;
     println!(
         "Listening on {}.{}.{}.{}:{}",
         ip[0], ip[1], ip[2], ip[3], port
     );
-    warp::serve(hello).run((ip, port)).await;
+    warp::serve(routes).run((ip, port)).await;
 }
+
+// Reason to use `(impl Reply,)`: https://github.com/seanmonstar/warp/issues/1012
+async fn build_routes() -> impl Filter<Extract = (impl Reply,)> + Clone {
+    let get_hello = warp::get()
+        .and(warp::path("hello"))
+        .and(warp::path::param::<String>())
+        .and(warp::path::end())
+        .and_then(greet);
+    let routes = get_hello;
+    routes
+}
+
+async fn greet(name: String) -> Result<impl warp::Reply, warp::Rejection> {
+    println!("Params: name = {}", name);
+    let response = format!("Hello, {}!", name);
+    Ok(warp::reply::json(&response))
+}
+
