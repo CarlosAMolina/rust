6a74728f624c553a96bd75b01c83f8a4f8447b87
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2023-08-24 13:29:43 +0200

    Change warp ip

--- warp/hello-world/hello-world/src/main.rs
+++ warp/hello-world/hello-world/src/main.rs
@@ -3,7 +3,7 @@ use warp::{http::StatusCode, Filter, Rejection, Reply};
 #[tokio::main]
 async fn main() {
     let routes = build_routes().await;
-    let ip = [127, 0 , 0, 1];
+    let ip = [0, 0 , 0, 0];
     let port = 3030;
     println!(
         "Listening on {}.{}.{}.{}:{}",
